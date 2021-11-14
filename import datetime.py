5a7a741385c19e3370e3dfc794ebd2b7a2263373
leorap07 <85707063+leorap07@users.noreply.github.com>
2021-11-14 00:19:36 -0500

    Add files via upload

---
+++ import datetime.py
@@ -0,0 +1,13 @@
+import datetime
+today = datetime.date.today()
+
+from datetime import date
+today = date.today
+
+import datetime as dt
+today = dt.date.today()
+
+from mymodule import MyClass as FirstClass
+from othermodule import Myclass as OtherClass
+
+from mymodule import *
